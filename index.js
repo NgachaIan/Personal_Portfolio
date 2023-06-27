59b5d513a4a9d0230c11406f72b96d597385232b
NgachaIan <ngacha.duncan@students.jkuat.ac.ke>
2023-06-28 00:06:15 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 function toggleMobileMenu() {
-    var menu = document.getElementById('mobileMenu');
-    var hamburgerMenu = document.querySelector('.hamburger-menu');
-    var closeIcon = document.querySelector('.close-icon');
-    var header = document.getElementById('header');
-    
+  var menu = document.getElementById('mobileMenu');
+  var hamburgerMenu = document.querySelector('.hamburger-menu');
+  var closeIcon = document.querySelector('.close-icon');
+  var header = document.getElementById('header');
 
-    if (menu.style.display === 'block') {
-      menu.style.display = 'none';
-      hamburgerMenu.style.display = 'block';
-      closeIcon.style.display = 'none';
-      header.style.paddingTop = '40px';
-    } else {
-      menu.style.display = 'block';
-      hamburgerMenu.style.display = 'none';
-      closeIcon.style.display = 'block';
-      header.style.paddingTop = '80px'; 
-    }
+  if (menu.classList.contains('open')) {
+    menu.classList.remove('open');
+    hamburgerMenu.style.display = 'block';
+    closeIcon.style.display = 'none';
+    header.style.paddingTop = '40px';
+  } else {
+    menu.classList.add('open');
+    hamburgerMenu.style.display = 'none';
+    closeIcon.style.display = 'block';
+    header.style.paddingTop = '80px';
   }
+}
 
-  function scrollToSection(sectionId) {
-    var section = document.querySelector(sectionId);
-    section.scrollIntoView({ behavior: 'smooth' });
-  }
+function scrollToSection(sectionId) {
+  var section = document.querySelector(sectionId);
+  section.scrollIntoView({ behavior: 'smooth' });
+}
