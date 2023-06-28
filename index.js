901cd9c3fc6a6d95f1f5391653a81b432843bf86
NgachaIan <ngacha.duncan@students.jkuat.ac.ke>
2023-06-28 18:20:26 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function toggleMobileMenu() {
   var closeIcon = document.querySelector('.close-icon');
   var header = document.getElementById('header');
 
-  if (menu.classList.contains('open')) {
-    menu.classList.remove('open');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
     hamburgerMenu.style.display = 'block';
     closeIcon.style.display = 'none';
     header.style.paddingTop = '40px';
   } else {
-    menu.classList.add('open');
+    menu.style.display = 'block';
     hamburgerMenu.style.display = 'none';
     closeIcon.style.display = 'block';
     header.style.paddingTop = '80px';
@@ -20,7 +20,30 @@ function toggleMobileMenu() {
 function scrollToSection(sectionId) {
   var section = document.querySelector(sectionId);
   section.scrollIntoView({ behavior: 'smooth' });
+
+  hideMobileMenu();
 }
+
+function hideMobileMenu() {
+  var menu = document.getElementById('mobileMenu');
+  var hamburgerMenu = document.querySelector('.hamburger-menu');
+  var closeIcon = document.querySelector('.close-icon');
+  var header = document.getElementById('header');
+
+  menu.style.display = 'none';
+  hamburgerMenu.style.display = 'block';
+  closeIcon.style.display = 'none';
+  header.style.paddingTop = '40px';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var navItems = document.querySelectorAll('.mobile-menu ul li');
+  for (var i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', function() {
+      scrollToSection(this.getAttribute('data-section'));
+    });
+  }
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   const projects = [
